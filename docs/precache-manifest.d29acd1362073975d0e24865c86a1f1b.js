4e420bea4a5bf200403109c37e696696ae3c76eb
Daniel Shwan <dannyshwan@gmail.com>
2020-04-26 19:01:24 -0400

    forgot to build

---
+++ docs/precache-manifest.d29acd1362073975d0e24865c86a1f1b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a74bd75450c0be20f427dc582ed4bce5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cae8af3764813c289292",
+    "url": "/static/css/main.b289cfc3.chunk.css"
+  },
+  {
+    "revision": "a2990824563d68df4cae",
+    "url": "/static/js/2.61a5f486.chunk.js"
+  },
+  {
+    "revision": "507c4436aa55e48136618e82a79cbf78",
+    "url": "/static/js/2.61a5f486.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cae8af3764813c289292",
+    "url": "/static/js/main.45fe3138.chunk.js"
+  },
+  {
+    "revision": "c5fee5381174b29127ef",
+    "url": "/static/js/runtime-main.866ef65a.js"
+  },
+  {
+    "revision": "0d11afb9c1e12ddea26bfa1a0d74d01c",
+    "url": "/static/media/Resume.0d11afb9.pdf"
+  },
+  {
+    "revision": "e0bf3d387d1531fc749b18d5f2135424",
+    "url": "/static/media/instagram.e0bf3d38.png"
+  },
+  {
+    "revision": "cc5ac81beb37dea015a49965e258b420",
+    "url": "/static/media/me.cc5ac81b.jpg"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.55406c9e2a460dc3e752ed144f6b3a91.js"
+  "/precache-manifest.d29acd1362073975d0e24865c86a1f1b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
