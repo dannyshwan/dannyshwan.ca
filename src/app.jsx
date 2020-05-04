62678ced3618cda082a531a290f5346b6454cc46
Daniel Shwan <dannyshwan@gmail.com>
2020-05-04 13:37:54 -0400

    redesigned about and skills page

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { ParticlesBackground } from './components/particles'
 import { Router } from './components/router';
 
 export class App extends React.Component{
@@ -7,7 +6,6 @@ export class App extends React.Component{
    render() {
       return(
          <div>
-            <ParticlesBackground/>
             <Router/>
          </div>
       );
--- src/views/skills.jsx
+++ src/views/skills.jsx
@@ -1,60 +1,62 @@
 import React from 'react';
 import '../styles/skills.scss';
-import { Footer } from '../components/footer';
+import Lottie from 'react-lottie';
+import * as animationData from '../assets/json/developer-boy.json';
 
 const style = {
-   display: "flex",
-   height: "85vh",
-   justifyContent: "center",
    fontFamily: '"Ubuntu", sansSerif',
+   display: "flex",
+   minHeight: "100vh",
+   justifyContent: "center",
    alignItems: "center",
-   flexFlow: "column",
-   textAlign: "center",
-   width: "100vw"
-};
-
-const languageProficiency = {
-   padding: "0.25em",
-   backgroundColor: "rgba(30, 127, 127, 0.80)",
-   borderRadius: "0.15em",
-   fontSize: "1em",
-   display: "inline-block"
 };
 
 export class Skills extends React.Component{
+   
    render(){
+      const defaultOptions = {
+         loop: true,
+         autoplay: true, 
+         animationData: animationData.default,
+         rendererSettings: {
+           preserveAspectRatio: 'xMidYMid slice'
+         }
+      };
+
       return(
          <section style={ style }>
             <div id='skills'>
-               <ul class='categories' id='coding'>
+               <ul className='categories' id='coding'>
                   <h1>Skills</h1>
                      <li>JavaScript</li><li>TypeScript</li><li>Java</li><li>Python</li>
                      <li>C</li><li>AWS</li><li>React.js</li><li>Vue.js</li><li>Node.js</li>
                      <li>Git</li><li>JQuery</li><li>HTML5/CSS3</li><li>Android</li><li>PHP</li>
                </ul>
-               <ul class='categories' id='lingustics'>
+               <ul className='categories' id='lingustics'>
                   <h1>Linguistics</h1>
                   <ul>
-                     <li><span style={ languageProficiency }>Fluent</span></li>
+                     <li><span className="languageProficiency">Fluent</span></li>
                      <li>English</li>
                   </ul>
                   <ul>
-                     <li><span style={ languageProficiency }>Intermediate</span></li>
+                     <li><span className="languageProficiency">Intermediate</span></li>
                      <li>French</li><li>Mandarin</li>
                   </ul>
                   <ul>
-                     <li><span style={ languageProficiency }>Basic</span></li>
+                     <li><span className="languageProficiency">Basic</span></li>
                      <li>Japanese</li>
                   </ul>
                </ul>
-               <ul class='categories' id='interests'>
+               <ul className='categories' id='interests'>
                   <h1>Interests</h1>
-                  <li>UI/UX Design</li><li>Artificial Intelligence</li><li>Mobile Development</li>
+                  <li>UI/UX Design</li><li>Artificial Intelligence</li><li>Full Stack Development</li>
                   <li>Product Development</li><li>Soccer</li><li>Volleyball</li><li>Running</li>
                   <li>Cooking</li><li>Sketching</li>
                </ul>
             </div>
-            <Footer/>
+            <div>
+               <Lottie options={ defaultOptions } height={400} width={400}/>
+            </div>
          </section>
       );
    };
